c4b3aa4ac7697683ae08d9ba24388b9ca4c5009e
727021 <1834544+727021@users.noreply.github.com>
2023-09-28 22:14:47 -0600

    testing nbt reader

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
 /* eslint-disable */
+import { readFile } from 'fs/promises'
+import { promisify } from 'util'
+import { gunzip } from 'zlib'
+
+import { read } from './nbt'
 import { Server } from './server'
 import * as log from './utils/debug'
 
-const server = Server.s
+// const server = Server.s
 
-const exit = async () => {
-  log.info('Shutting down...')
-  await server.stop()
-  process.exit()
-}
+// const exit = async () => {
+//   log.info('Shutting down...')
+//   await server.stop()
+//   process.exit()
+// }
 
-process.once('SIGINT', exit)
-process.once('SIGTERM', exit)
+// process.once('SIGINT', exit)
+// process.once('SIGTERM', exit)
 
-process.on('unhandledRejection', e => log.error(e))
+// process.on('unhandledRejection', e => log.error(e))
 
-server.start()
+// server.start()
+read('../Player-nan-value.dat').then(nbt => {
+  console.log(nbt)
+})
--- src/nbt/index.ts
+++ src/nbt/index.ts
@@ -1,4 +1,7 @@
-/* eslint-disable */
+import { readFile } from 'fs/promises'
+import { promisify } from 'util'
+import { gunzip } from 'zlib'
+
 enum TagType {
   END = 0,
   BYTE = 1,
@@ -15,11 +18,41 @@ enum TagType {
   LONG_ARRAY = 12
 }
 
+type NBTTagRaw = {
+  start: number
+  end: number
+  tag: {
+    typeId: TagType
+    name?: string
+    payload?:
+      | number
+      | string
+      | bigint
+      | number[]
+      | bigint[]
+      | NBTTagRaw[]
+      | { [key: string]: NBTTagRaw }
+  }
+}
+
+type NBTTag = {
+  typeId: TagType
+  name?: string
+  payload?:
+    | number
+    | string
+    | bigint
+    | number[]
+    | bigint[]
+    | NBTTag[]
+    | { [key: string]: NBTTag }
+}
+
 const readTag = (
   buf: Buffer,
   start = 0,
   typeId: number | undefined = undefined
-) => {
+): NBTTagRaw => {
   let offset = start
   let named = false
   if (typeId === undefined) {
@@ -160,7 +193,7 @@ const readTag = (
           end: offset + 4,
           tag: {
             typeId,
-            payload
+            payload: isNaN(payload) ? 0 : payload
           }
         }
       }
@@ -176,7 +209,7 @@ const readTag = (
         tag: {
           typeId,
           name,
-          payload
+          payload: isNaN(payload) ? 0 : payload
         }
       }
     }
@@ -188,7 +221,7 @@ const readTag = (
           end: offset + 8,
           tag: {
             typeId,
-            payload
+            payload: isNaN(payload) ? 0 : payload
           }
         }
       }
@@ -204,7 +237,7 @@ const readTag = (
         tag: {
           typeId,
           name,
-          payload
+          payload: isNaN(payload) ? 0 : payload
         }
       }
     }
@@ -225,7 +258,9 @@ const readTag = (
         }
       }
       const nameLength = buf.readUInt16BE(offset)
-      const name = buf.subarray(offset + 2, offset + 2 + nameLength)
+      const name = buf
+        .subarray(offset + 2, offset + 2 + nameLength)
+        .toString('utf-8')
       const length = buf.readInt32BE(offset + 2 + nameLength)
       const payload = []
       for (let i = 0; i < length; ++i) {
@@ -245,7 +280,9 @@ const readTag = (
     case TagType.STRING: {
       if (!named) {
         const length = buf.readUInt16BE(offset)
-        const payload = buf.subarray(offset + 2, offset + 2 + length)
+        const payload = buf
+          .subarray(offset + 2, offset + 2 + length)
+          .toString('utf-8')
         return {
           start,
           end: offset + 2 + length,
@@ -277,8 +314,116 @@ const readTag = (
         }
       }
     }
-    case TagType.LIST:
-    case TagType.COMPOUND:
+    case TagType.LIST: {
+      if (!named) {
+        const tagType = buf.readInt8(offset)
+        const length = buf.readInt32BE(offset + 1)
+        if (tagType === TagType.END && length > 0) {
+          throw new Error(`NBTError: Invalid list tag at ${offset}`)
+        }
+        const payload: NBTTagRaw[] = []
+        for (let i = 0; i < length; ++i) {
+          payload.push(readTag(buf, payload.at(-1)?.end ?? offset + 5, tagType))
+        }
+        return {
+          start,
+          end: payload.at(-1)?.end ?? offset + 5,
+          tag: {
+            typeId,
+            payload
+          }
+        }
+      }
+      const nameLength = buf.readUInt16BE(offset)
+      const name = buf
+        .subarray(offset + 2, offset + 2 + nameLength)
+        .toString('utf-8')
+      const tagType = buf.readInt8(offset + 2 + nameLength)
+      const length = buf.readInt32BE(offset + 2 + nameLength + 1)
+      if (tagType === TagType.END && length > 0) {
+        throw new Error(`NBTError: Invalid list tag at ${start}`)
+      }
+      const payload: NBTTagRaw[] = []
+      for (let i = 0; i < length; ++i) {
+        payload.push(
+          readTag(
+            buf,
+            payload.at(-1)?.end ?? offset + 2 + nameLength + 5,
+            tagType
+          )
+        )
+      }
+      return {
+        start,
+        end: payload.at(-1)?.end ?? offset + 2 + nameLength + 5,
+        tag: {
+          typeId,
+          name,
+          payload
+        }
+      }
+    }
+    case TagType.COMPOUND: {
+      if (!named) {
+        const payload: { [key: string]: NBTTagRaw } = {}
+        let prevTag
+        // eslint-disable-next-line no-constant-condition
+        while (true) {
+          prevTag = readTag(buf, prevTag?.end ?? offset)
+          if (prevTag?.tag?.typeId === TagType.END) {
+            return {
+              start,
+              end: prevTag?.end,
+              tag: {
+                typeId,
+                payload
+              }
+            }
+          }
+          if (!prevTag?.tag?.name) {
+            throw new Error(
+              `NBTError: Nameless tag in compound tag at ${start}`
+            )
+          }
+          if (prevTag?.tag?.name in payload) {
+            throw new Error(
+              `NBTError: Duplicate tag name '${prevTag.tag.name}' in compound tag at ${start}`
+            )
+          }
+          payload[prevTag.tag.name] = prevTag
+        }
+      }
+      const nameLength = buf.readUInt16BE(offset)
+      const name = buf
+        .subarray(offset + 2, offset + 2 + nameLength)
+        .toString('utf-8')
+      const payload: { [key: string]: NBTTagRaw } = {}
+      let prevTag
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
+        prevTag = readTag(buf, prevTag?.end ?? offset + 2 + nameLength)
+        if (prevTag?.tag?.typeId === TagType.END) {
+          return {
+            start,
+            end: prevTag?.end,
+            tag: {
+              typeId,
+              name,
+              payload
+            }
+          }
+        }
+        if (!prevTag?.tag?.name) {
+          throw new Error(`NBTError: Nameless tag in compound tag at ${start}`)
+        }
+        if (prevTag?.tag?.name in payload) {
+          throw new Error(
+            `NBTError: Duplicate tag name '${prevTag.tag.name}' in compound tag at ${start}`
+          )
+        }
+        payload[prevTag.tag.name] = prevTag
+      }
+    }
     case TagType.INT_ARRAY: {
       if (!named) {
         const length = buf.readInt32BE(offset)
@@ -296,7 +441,9 @@ const readTag = (
         }
       }
       const nameLength = buf.readUInt16BE(offset)
-      const name = buf.subarray(offset + 2, offset + 2 + nameLength)
+      const name = buf
+        .subarray(offset + 2, offset + 2 + nameLength)
+        .toString('utf-8')
       const length = buf.readInt32BE(offset + 2 + nameLength)
       const payload = []
       for (let i = 0; i < length; ++i) {
@@ -330,7 +477,9 @@ const readTag = (
         }
       }
       const nameLength = buf.readUInt16BE(offset)
-      const name = buf.subarray(offset + 2, offset + 2 + nameLength)
+      const name = buf
+        .subarray(offset + 2, offset + 2 + nameLength)
+        .toString('utf-8')
       const length = buf.readInt32BE(offset + 2 + nameLength)
       const payload = []
       for (let i = 0; i < length; ++i) {
@@ -348,13 +497,40 @@ const readTag = (
       }
     }
   }
-  return null
+  throw new Error(`NBTError: Invalid tag type id '${typeId}' at ${start}`)
 }
 
-export const read = (buf: Buffer) => {
-  let offset = 0
+const isGzipped = (buf: Buffer) => buf[0] === 0x1f && buf[1] === 0x8b
 
-  let data = {}
+const normalizeNBT = (raw: NBTTagRaw): NBTTag => {
+  switch (raw.tag.typeId) {
+    case TagType.LIST:
+      return {
+        ...raw.tag,
+        payload: (raw.tag.payload as NBTTagRaw[]).map(tag => normalizeNBT(tag))
+      }
+    case TagType.COMPOUND:
+      return {
+        ...raw.tag,
+        payload: Object.entries(
+          raw.tag.payload as { [key: string]: NBTTagRaw }
+        ).reduce(
+          (acc, [key, val]) => ({
+            ...acc,
+            [key]: normalizeNBT(val)
+          }),
+          {}
+        )
+      }
+  }
 
-  return
+  return raw.tag as NBTTag
+}
+
+export const read = async (path: string): Promise<NBTTag> => {
+  const file = await readFile(path)
+  const nbtBuffer = isGzipped(file) ? await promisify(gunzip)(file) : file
+  const nbtRaw = readTag(nbtBuffer)
+  const nbt = normalizeNBT(nbtRaw)
+  return nbt
 }
